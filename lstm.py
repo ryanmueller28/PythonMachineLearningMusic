38018efaa1d459b51cf867e89c3b868a54530952
ryamnmueller28 <ryan.mueller@doane.edu>
2020-12-10 14:35:08 -0600

    Script for training and scrubbed midi files for dataset

---
+++ lstm.py
@@ -0,0 +1,130 @@
+import glob
+import pickle
+import numpy
+from music21 import converter, instrument, note, chord
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Activation
+from keras.layers import BatchNormalization as BatchNorm
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+
+
+# Train the neural net to generate music
+def train_network():
+    notes = get_musical_notes()
+
+    # Get Pitch names
+    n_vocab = len(set(notes))
+
+    network_input, network_output = prepare_neural_net_sequence(notes, n_vocab)
+
+    model = create_neural_net(network_input, n_vocab)
+
+    train(model, network_input, network_output)
+
+
+# Read notes
+def get_musical_notes():
+    musical_notes = []
+
+    for file in glob.glob("midi_songs/*.mid"):
+        midi = converter.parse(file)
+
+        print("Parsing: %s" % file)
+
+        notes_to_parse = None
+
+        # Instruments?
+        try:
+            s2 = instrument.partitionByInstrument(midi)
+            notes_to_parse = s2.parts[0].recurse()
+        except:
+            notes_to_parse = midi.flat.notes
+
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                musical_notes.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                musical_notes.append('.'.join(str(n) for n in element.normalOrder))
+
+    with open('data/notes', 'wb') as filepath:
+        pickle.dump(musical_notes, filepath)
+
+    return musical_notes
+
+
+# prepare sequence of notes
+def prepare_neural_net_sequence(notes, n_vocab):
+    sequence_length = 100
+    pitch_names = sorted(set(item for item in notes))
+
+    note_to_int = dict((note, number) for number, note in enumerate(pitch_names))
+
+    network_input = []
+    network_output = []
+
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+
+    # Create LTSM compatible format
+    network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
+
+    # Network input normalize
+    network_input = network_input / float(n_vocab)
+
+    network_output = np_utils.to_categorical(network_output)
+
+    return (network_input, network_output)
+
+
+# Create Neural net
+def create_neural_net(network_input, n_vocab):
+    model = Sequential()
+
+    model.add(LSTM(
+        512,
+        input_shape=(network_input.shape[1], network_input.shape[2]),
+        recurrent_dropout=0.3,
+        return_sequences=True
+    ))
+
+    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3))
+    model.add(LSTM(512))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(n_vocab))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    return model
+
+
+# train neural net on reading music
+def train(model, network_input, network_output):
+    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min'
+    )
+
+    callbacks_list = [checkpoint]
+    model.fit(network_input, network_output, epochs=200, batch_size=128, callbacks=callbacks_list)
+
+
+if __name__ == "__main__":
+    train_network()
